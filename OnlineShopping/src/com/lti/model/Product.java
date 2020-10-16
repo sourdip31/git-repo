5f50c5c762e8511e43e160ac1b37fc336b1be7c7
sourdip31 <72907752+sourdip31@users.noreply.github.com>
2020-10-16 10:16:39 +0530

    updated product.java thru br1

--- OnlineShopping/src/com/lti/model/Product.java
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -5,6 +5,11 @@ public class Product {
 	int productId ;
 	String productName ;
 	String productDescription ;
+	int quantity ;
+	
+	public int getQuantity(){
+		return quantity ; 
+	
 	public int getProductId() {
 		return productId;
 	}
